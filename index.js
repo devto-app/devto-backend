dd268b45a92adffc2e040c81b7652c79cd342b91
Víctor Cosme <victorcosmema@gmail.com>
2023-08-26 02:22:16 -0600

    Connect to db

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 const app = require('./src/server');
 const port = 8080;
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port} 🔊`);
-});
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose
+    .connect(URL)
+    .then(() => {
+        console.log('Dev.to DB connected 🟢');
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port} 🔊`);
+        });
+    })
+    .catch(() => {
+        console.log('Oooops, smth went wrong 🔴');
+    });
