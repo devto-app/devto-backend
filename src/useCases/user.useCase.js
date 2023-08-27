df6171c19b74610e6d610a4fbb454fde489366c8
Víctor Cosme <victorcosmema@gmail.com>
2023-08-27 02:08:05 -0600

    Add getAll and create: useCase + router

--- src/useCases/user.useCase.js
+++ src/useCases/user.useCase.js
@@ -6,8 +6,8 @@ async function getAll() {
     return await User.find();
 }
 
-function create(userData) {
-    const newUser = User.create(userData);
+async function create(userData) {
+    const newUser = await User.create(userData);
     return newUser;
 }
 
