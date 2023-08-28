6f57b77b1691e298ed5501fa6fbd8392fccfd96e
Víctor Cosme <victorcosmema@gmail.com>
2023-08-27 19:34:06 -0600

    Add bcrypt library, encrypt passwords

---
+++ src/lib/bcrypt.js
@@ -0,0 +1,13 @@
+const bcryptjs = require('bcryptjs');
+
+const saltRound = 10;
+
+function encrypt(text) {
+    return bcryptjs.hashSync(text, saltRound);
+}
+
+function verify(hash, text) {
+    return bcryptjs.compareSync(text, hash);
+}
+
+module.exports = { encrypt, verify };
--- src/useCases/user.useCase.js
+++ src/useCases/user.useCase.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const User = require('../models/user.model');
 const createError = require('http-errors');
+const bcrypt = require('../lib/bcrypt');
 
 async function getAll() {
     return await User.find();
@@ -10,6 +11,15 @@ async function getAll() {
 async function create(userData) {
     const userExists = await User.findOne({ email: userData.email });
     if (userExists) throw new createError(412, 'User already exists');
+
+    const passwordRegExp = new RegExp(
+        '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$'
+    );
+    if (!passwordRegExp.test(userData.password)) {
+        throw new createError(400, 'Weak password');
+    }
+    userData.password = bcrypt.encrypt(userData.password);
+
     const newUser = await User.create(userData);
     return newUser;
 }
